de96ac61efe36785bee48480940c9deced854bef
JoahanVillalobos <joahan.ortega@cceo.com.mx>
2024-02-27 19:22:53 -0600

    feature(App.vue): :sparkles: add style to h2 label

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 const name = 'Vue dinámico'
+const colorStyle = "color: blue"
 </script>
 
 <template>
   <h1>Hola {{ name.toUpperCase() }}!</h1>
+  <h2 v-bind:style="colorStyle">soy azul</h2>
 </template>
 
 <style>
